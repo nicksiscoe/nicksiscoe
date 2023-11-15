49b7e2d358e6ad937ceff7b929d814bae5ea8905
nick siscoe <nick.siscoe@gmail.com>
2023-11-15 15:55:42 -0600

    add more sports

--- app/api/experiments/nfl-tiktok/route.ts
+++ app/api/experiments/nfl-tiktok/route.ts
@@ -1,31 +1,62 @@
 import { NextResponse } from "next/server";
 
+function shuffle(array: any[]) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
 export async function GET() {
-  const articleRes = await fetch(
-    "http://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=100"
-  );
-  const articleResponse = await articleRes.json();
-  const articles = articleResponse.articles.filter(
-    (article: any) => !!article?.links?.api?.news?.href
-  );
-  const videos: any[] = (
+  const urls = [
+    "http://site.api.espn.com/apis/site/v2/sports/football/nfl/news?limit=20",
+    "http://site.api.espn.com/apis/site/v2/sports/football/college-football/news?limit=20",
+    "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news?limit=20",
+    "http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/news?limit=20",
+    "http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/news?limit=20",
+  ];
+  const videos = (
     await Promise.all(
-      articles.map(async (article: any) => {
-        const res = await fetch(article.links.api.news.href);
-        const response = await res.json();
-        // console.log(response);
-        return {
-          caption: article.headline,
-          urls: response.videos?.map(
-            (video: any) =>
-              video?.links?.source?.HD?.href ||
-              video?.links?.source?.SD?.href ||
-              video?.links?.source?.href
-          ),
-        };
+      urls.map(async (url) => {
+        const articleRes = await fetch(url);
+        const articleResponse = await articleRes.json();
+        const articles = articleResponse.articles.filter(
+          (article: any) => !!article?.links?.api?.news?.href
+        );
+        return (
+          await Promise.all(
+            articles.map(async (article: any) => {
+              const res = await fetch(article.links.api.news.href);
+              const response = await res.json();
+              // console.log(response);
+              return {
+                caption: article.headline,
+                urls: response.videos?.map(
+                  (video: any) =>
+                    video?.links?.source?.HD?.href ||
+                    video?.links?.source?.SD?.href ||
+                    video?.links?.source?.href
+                ),
+              };
+            })
+          )
+        ).filter((u) => !!u.urls) as any[];
       })
     )
-  ).filter((u) => !!u.urls) as any[];
+  ).flat(1);
 
-  return NextResponse.json(videos);
+  return NextResponse.json(shuffle(videos));
 }
